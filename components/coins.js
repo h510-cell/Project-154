80b152fbb43b3b11cea712a8b787f7692a0a63e3
h510-cell <58731327+h510-cell@users.noreply.github.com>
2021-01-19 11:51:37 +0530

    Add files via upload

---
+++ components/coins.js
@@ -0,0 +1,35 @@
+// Registering component in coins.js
+
+AFRAME.registerComponent("target-coins", {
+    init: function () {
+      for (var i = 1; i <= 20; i++) {
+        //id
+        var id = `coin${i}`;
+  
+        //position variables     
+        var posX =(Math.random() * 3000 + (-1000));      
+        var posY = (Math.random() * 2 + (-1));
+        var posZ = (Math.random() * 3000 + -1000);
+  
+        var position = { x: posX, y: posY, z: posZ };
+  
+        //call the function
+        this.createCoins(id, position);
+      }
+    } ,
+  
+    createRings: function(id,position) {
+  
+      var islandEl = document.querySelector("#island");
+  
+      var coinEl = document.createElement("a-entity");
+  
+      ringEl.setAttribute("id",id);
+      ringEl.setAttribute("material","color","#ff9100");
+      ringEl.setAttribute("position",position);
+      ringEl.setAttribute("geometry",{ primitive: "torus",radius: 8 });   
+  
+      terrainEl.appendChild(ringEl);
+    }
+  });
+  
